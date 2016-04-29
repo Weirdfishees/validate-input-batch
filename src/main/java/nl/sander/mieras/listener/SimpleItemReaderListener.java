c5bde886193eb9938a215307f50b98d5398a0733
Sander Mieras <Sander@192.168.178.15>
2016-04-29 12:40:08 +0200

    Applied DecimalFormat to improve readability of record count

--- src/main/java/nl/sander/mieras/listener/SimpleItemReaderListener.java
+++ src/main/java/nl/sander/mieras/listener/SimpleItemReaderListener.java
@@ -1,5 +1,7 @@
 package nl.sander.mieras.listener;
 
+import java.text.DecimalFormat;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.ItemReadListener;
@@ -7,21 +9,24 @@ import org.springframework.batch.core.ItemReadListener;
 public class SimpleItemReaderListener<Item> implements ItemReadListener<Item>{
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SimpleItemReaderListener.class);
-	private static final double NANO_TO_SECOND = 1_000_000_000.0;
-	
-	private long start;
+	private static final int GROUP_SIZE = 3;
+	private static final double NANO_TO_SECOND = 1_000_000_000.0;	
+	private static final String PATTERN = ",###";
 	
 	private int startCount = 0;
 	private int logInterval = 1000;
 	private int currentCount;
 	private int totalCount;
 	private long timeElapsed;
+	private long startTime;
+	private DecimalFormat decimalFormat = new DecimalFormat(PATTERN);
 
 	@Override
-	public void afterRead(Item item) {		
-		timeElapsed += System.nanoTime() - start;		
-		if (currentCount == logInterval) {
-			LOG.info(String.format("Read records [ %d ] to [ %d ] in average %.2f seconds", startCount, totalCount, timeElapsed / NANO_TO_SECOND));
+	public void afterRead(Item item) {
+		decimalFormat.setGroupingSize(GROUP_SIZE);
+		timeElapsed += System.nanoTime() - startTime;		
+		if (currentCount == logInterval) {			
+			LOG.info(String.format("Read records [ %s ] to [ %s ] in average %.2f seconds", decimalFormat.format(startCount), decimalFormat.format(totalCount), timeElapsed / NANO_TO_SECOND));
 			startCount += currentCount;
 			currentCount = 0;
 			timeElapsed = 0;
@@ -33,7 +38,7 @@ public class SimpleItemReaderListener<Item> implements ItemReadListener<Item>{
 
 	@Override
 	public void beforeRead() {
-		start = System.nanoTime();				
+		startTime = System.nanoTime();				
 	}
 
 	@Override
