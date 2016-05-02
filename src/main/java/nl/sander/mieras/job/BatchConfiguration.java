de2e38791d2cb94e42fca0a67386992c1be8abfa
Sander Mieras <Sander@192.168.178.15>
2016-05-02 19:22:33 +0200

    adjusted to only incorporate 1 file

--- src/main/java/nl/sander/mieras/job/BatchConfiguration.java
+++ src/main/java/nl/sander/mieras/job/BatchConfiguration.java
@@ -29,136 +29,126 @@ import nl.sander.mieras.validator.BeanValidator;
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
-	
+
 	private static final String INPUT_FILE = "rankings.csv";
 	private static final String TARGET_FILE = "target/invalidRecord.csv";
 
-    @Autowired
-    public JobBuilderFactory jobBuilderFactory;
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
 
-    @Autowired
-    public StepBuilderFactory stepBuilderFactory;
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
 
-    @Bean
+	@Bean
 	public Job importUserJob() {
-	    return jobBuilderFactory.get("importUserJob")            
-	            .flow(validateInput())
-	            .end()
-	            .build();
+		return jobBuilderFactory.get("importUserJob")
+				.flow(validateInput())
+				.end()
+				.build();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Bean
 	public Step validateInput() {
-	    return stepBuilderFactory.get("validateInput")
-	            .chunk(100_000)	           
-	            .reader(reader())	            
-	            .listener(readerListener())
-	            .processor(processor())
-	            .listener(processorListener())
-	            .writer(writer())
-	            .listener(invalidItemListener())
-	            .build();
+		return stepBuilderFactory.get("validateInput")
+				.chunk(100_000)
+				.reader(reader())
+				.listener(readerListener())
+				.processor(processor())
+				.listener(processorListener())
+				.writer(writer())
+				.listener(invalidItemListener())
+				.build();
 	}
-	
-	// Reader - Processor - Writer	
+
+	// Reader - Processor - Writer
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Bean	
-    public FlatFileItemReader reader() {
-        FlatFileItemReader reader = new FlatFileItemReader();        
-        reader.setLinesToSkip(1);        
-        reader.setSkippedLinesCallback(tokenizeHeader());
-        reader.setResource(getInputFile());
-        reader.setLineMapper(new DefaultLineMapper() {{
-            setLineTokenizer(tokenizeHeader());
-            setFieldSetMapper(new BeanWrapperFieldSetMapper<Ranking>() {{
-                setTargetType(Ranking.class);
-            }});
-        }});       
-        return reader;
-    }	
-	
-	/*@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Bean	
-	public FlatFileItemReader personReader() {
-		FlatFileItemReader reader = new FlatFileItemReader();        
-		reader.setLinesToSkip(1);        
+	@Bean
+	public FlatFileItemReader reader() {
+		FlatFileItemReader reader = new FlatFileItemReader();
+		reader.setLinesToSkip(1);
 		reader.setSkippedLinesCallback(tokenizeHeader());
 		reader.setResource(getInputFile());
-		reader.setLineMapper(new DefaultLineMapper() {{
-			setLineTokenizer(tokenizeHeader());
-			setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
-				setTargetType(Person.class);
-			}});
-		}});       
+		reader.setLineMapper(new DefaultLineMapper() {
+			{
+				setLineTokenizer(tokenizeHeader());
+				setFieldSetMapper(new BeanWrapperFieldSetMapper<Ranking>() {
+					{
+						setTargetType(Ranking.class);
+					}
+				});
+			}
+		});
 		return reader;
-	}*/	
+	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Bean
-	public PassThroughValidatingItemProcessor processor(){
+	public PassThroughValidatingItemProcessor processor() {
 		PassThroughValidatingItemProcessor processor = new PassThroughValidatingItemProcessor<>();
 		processor.setValidator(validator());
 		return processor;
 	}
-    
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Bean
-	public FlatFileItemWriter writer(){
+	public FlatFileItemWriter writer() {
 		FlatFileItemWriter writer = new FlatFileItemWriter();
 		writer.setResource(new FileSystemResource(new File(TARGET_FILE)));
-		writer.setLineAggregator(aggregateHeader());		
+		writer.setLineAggregator(aggregateHeader());
 		return writer;
-	}    
-    
-    // Support Beans	
-	@Bean
-	public ClassPathResource getInputFile(){
-		return new ClassPathResource(INPUT_FILE);		
 	}
-	
-    @Bean 
-    public HeaderTokenizer tokenizeHeader(){
-    	HeaderTokenizer tokenizer = new HeaderTokenizer();
-    	//optional setting, custom delimiter is set to ','
-    	//tokenizer.setDelimiter(",");
-    	tokenizer.setInputResource(getInputFile());
-    	return tokenizer;
-    }
-    
-    @Bean
-    public HeaderAggregator aggregateHeader(){
-    	HeaderAggregator aggregator = new HeaderAggregator();
-    	aggregator.setHeaderTokenizer(tokenizeHeader());
-    	return aggregator;
-    }
-    
-    @Bean
-    public BeanValidator validator(){
-    	BeanValidator validator = new BeanValidator();
-    	//optional setting, logging is disable by standard
-    	//validator.setEnableLogging(true);
-    	return validator;
-    }    
-    
-    @Bean
-    public ConcreteItemReaderListener readerListener(){
-    	ConcreteItemReaderListener listener = new ConcreteItemReaderListener();
-    	//optional setting, custom logging is set to 1000, increase for less verbose logging
-    	listener.setLogInterval(100000);
-    	return listener;
-    }
-    
-    @Bean
-    public ConcreteItemProcessorListener processorListener(){
-    	ConcreteItemProcessorListener listener = new ConcreteItemProcessorListener();
-    	//optional setting, custom logging is set to 1000, increase for less verbose logging
-    	listener.setLogInterval(100000);
-    	return listener;
-    }
-    
-    @Bean
-    public InvalidItemStepExecutionListener invalidItemListener(){
-    	return new InvalidItemStepExecutionListener();    	
-    }
+
+	// Support Beans
+	@Bean
+	public ClassPathResource getInputFile() {
+		return new ClassPathResource(INPUT_FILE);
+	}
+
+	@Bean
+	public HeaderTokenizer tokenizeHeader() {
+		HeaderTokenizer tokenizer = new HeaderTokenizer();
+		// optional setting, custom delimiter is set to ','
+		// tokenizer.setDelimiter(",");
+		tokenizer.setInputResource(getInputFile());
+		return tokenizer;
+	}
+
+	@Bean
+	public HeaderAggregator aggregateHeader() {
+		HeaderAggregator aggregator = new HeaderAggregator();
+		aggregator.setHeaderTokenizer(tokenizeHeader());
+		return aggregator;
+	}
+
+	@Bean
+	public BeanValidator validator() {
+		BeanValidator validator = new BeanValidator();
+		// optional setting, logging is disable by standard
+		// validator.setEnableLogging(true);
+		return validator;
+	}
+
+	@Bean
+	public ConcreteItemReaderListener readerListener() {
+		ConcreteItemReaderListener listener = new ConcreteItemReaderListener();
+		// optional setting, custom logging is set to 1000, increase for less
+		// verbose logging
+		listener.setLogInterval(100000);
+		return listener;
+	}
+
+	@Bean
+	public ConcreteItemProcessorListener processorListener() {
+		ConcreteItemProcessorListener listener = new ConcreteItemProcessorListener();
+		// optional setting, custom logging is set to 1000, increase for less
+		// verbose logging
+		listener.setLogInterval(100000);
+		return listener;
+	}
+
+	@Bean
+	public InvalidItemStepExecutionListener invalidItemListener() {
+		return new InvalidItemStepExecutionListener();
+	}
 }
